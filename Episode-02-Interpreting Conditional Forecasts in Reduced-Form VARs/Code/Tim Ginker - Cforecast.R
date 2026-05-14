7a4dab0c50b51e9b31497a2d406351dc98cffdaf
Juan Nicolas D'Amico <econlabwithexperts@gmail.com>
2026-05-14 00:36:23 -0400

    Update Tim Ginker - Cforecast.R

--- Episode-02-Interpreting Conditional Forecasts in Reduced-Form VARs/Code/Tim Ginker - Cforecast.R
+++ Episode-02-Interpreting Conditional Forecasts in Reduced-Form VARs/Code/Tim Ginker - Cforecast.R
@@ -16,9 +16,9 @@
 #
 # Additional interview materials and citation information are available at:
 # https://github.com/juan-damico/EconLab-with-the-Experts/tree/main/Episode-02-Interpreting%20Conditional%20Forecasts%20in%20Reduced-Form%20VARs
-# install.packages("devtools")
 
 # Installation
+# install.packages("devtools")
 devtools::install_github("timginker/cforecast")
 
 suppressPackageStartupMessages({
